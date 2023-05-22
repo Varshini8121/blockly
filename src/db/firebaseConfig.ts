99b1d3783f80cccb843f8f0fbfaa1aefc8c35d47
Kumaresh <79594917+Kumaresh1@users.noreply.github.com>
2023-05-22 22:19:16 +0530

    firebase config updated

--- src/db/firebaseConfig.ts
+++ src/db/firebaseConfig.ts
@@ -9,13 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC-TmfZ1Fpl5-hsrEWt5bP6GT63Ab4eMao",
-  authDomain: "blockly-project-22b6d.firebaseapp.com",
-  projectId: "blockly-project-22b6d",
-  storageBucket: "blockly-project-22b6d.appspot.com",
-  messagingSenderId: "473678429706",
-  appId: "1:473678429706:web:f8aeab018719ce2c6f69f3",
-  measurementId: "G-0KMCNR6F9V",
+  apiKey: "AIzaSyCIjBXxjefZHLVwXTVODRKCZdaKNejbTFQ",
+  authDomain: "fyp-project-fc647.firebaseapp.com",
+  projectId: "fyp-project-fc647",
+  storageBucket: "fyp-project-fc647.appspot.com",
+  messagingSenderId: "652326938482",
+  appId: "1:652326938482:web:c2ade6b67d0cda0727dfeb",
 };
 
 // Initialize Firebase
